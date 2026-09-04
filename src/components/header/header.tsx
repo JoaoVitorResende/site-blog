348a59474ac49a6a2e338443bb2cacd943042822
João <joaovitorresende.jr@gmail.com>
2026-09-04 18:43:59 -0300

    chore: adding style guide

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -9,14 +9,12 @@ export const Header = () => {
         <header className="fixed top-0 z-50 w-full border-b border-white/10 bg-background/95 backdrop-blur supports-[backdrop-filters]:bg-background/60">
             <div className="mx-auto max-w-7xl px-4 sm:px-6 ls:px-80">
                 <div className="flex h-16 items-center justify-between">
-                    <Link href="/" >
-                        <Logo/>
-                    </Link>
+                    <Logo />
                     <nav className="flex items-center gap-6">
                         <ActiveLink href="/">Inicio</ActiveLink>
                         <ActiveLink href="/blog">Blog</ActiveLink>
                         <Button variant="secondary" asChild>
-                           <Link href="/comecar">Começar</Link>
+                            <Link href="/comecar">Começar</Link>
                         </Button>
                     </nav>
                 </div>
