4c7043477b527e2e17fa9024b45e0a80923cf02b
João <joaovitorresende.jr@gmail.com>
2026-09-04 10:24:51 -0300

    chore: atualizando para comecar o projeto

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
-import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="min-h-screen min-w-full bg-slate-500">
-      <Link href="/Images">Pagina imagem</Link>
+      <Button variant="secondary">bt</Button>
     </div>
   );
 }
