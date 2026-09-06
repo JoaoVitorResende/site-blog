4bb41b17c0e7f4bb73784fc41928b87a5a14cb7a
João <joaovitorresende.jr@gmail.com>
2026-09-06 10:02:49 -0300

    feat: customer story section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { CustomerStorySection } from "@/components/custumer-story-section";
 import { FeatureSection } from "@/components/feature-section";
 import { HeroSection } from "@/components/hero";
 import { SuportSection } from "@/components/suport-section";
@@ -9,6 +10,7 @@ export default function Home() {
         <HeroSection />
         <FeatureSection/>
         <SuportSection/>
+        <CustomerStorySection/>
       </article>
     </>
   );
