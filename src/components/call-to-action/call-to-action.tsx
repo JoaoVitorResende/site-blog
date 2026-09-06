0448ca5f1a0494ad2eb3cb2df56d8c86cd8c17b2
João <joaovitorresende.jr@gmail.com>
2026-09-06 18:29:35 -0300

    feat: adding background with image

--- src/components/call-to-action/call-to-action.tsx
+++ src/components/call-to-action/call-to-action.tsx
@@ -3,8 +3,9 @@ import { Store } from "lucide-react"
 
 export const CallToAction = () => {
     return (
-        <section className="py-24 bg-gradient-to-b from-cyan-950/20 to-gray-700">
-            <div className="container">
+        <section className="relative py-24 bg-gradient-to-b from-cyan-950/20 to-gray-700">
+            <div className="absolute inset-0 hidden md:block bg-[url('/backgroundFooter.svg')] bg-cover bg-center bg-no-repeat opacity-90" />
+            <div className="container relative">
                 <div className="flex flex-col items-center gap-6 text-center">
                     <div className="p-4 bg-cyan-300 w-fit rounded-full">
                         <Store className="text-cyan-100"/>
