5baa17d6ca04ea594cd4ebdbb296991759ca55c2
João <joaovitorresende.jr@gmail.com>
2026-09-08 09:38:31 -0300

    feat: creating search component

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,3 +1,5 @@
+import { Search } from "@/components/search";
+
 export default function BlogPage() {
     return (
         <div className="flex flex-col py-24 flex-grow h-full">
@@ -9,6 +11,7 @@ export default function BlogPage() {
                             Dicas e estrategias para impulsionar seu negocio
                         </h1>
                     </div>
+                    <Search />
                 </div>
             </header>
         </div>
