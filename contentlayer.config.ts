9fb0c7e825342b59c0f02b5d97a2e0c23efbc7c0
João <joaovitorresende.jr@gmail.com>
2026-09-08 15:33:18 -0300

    feat: configuring to show all posts

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,5 +1,22 @@
-// contentlayer.config.ts
-import { defineDocumentType, makeSource } from 'contentlayer2/source-files'
+import {
+  defineDocumentType,
+  defineNestedType,
+  makeSource,
+} from 'contentlayer2/source-files';
+
+const Author = defineNestedType(() => ({
+  name: 'Author',
+  fields: {
+    name: {
+      type: 'string',
+      required: true,
+    },
+    avatar: {
+      type: 'string',
+      required: true,
+    },
+  },
+}));
 
 export const Post = defineDocumentType(() => ({
   name: 'Post',
@@ -9,13 +26,18 @@ export const Post = defineDocumentType(() => ({
     date: { type: 'date', required: true },
     description: { type: 'string', required: true },
     image: { type: 'string', required: true },
+    author: {
+      type: 'nested',
+      of: Author,
+      required: true,
+    },
   },
   computedFields: {
-    slug:{
-        type: 'string',
-        resolve:(doc) => doc._raw.sourceFileName.replace('.md','')
-    }
+    slug: {
+      type: 'string',
+      resolve: (doc) => doc._raw.sourceFileName.replace('.md', ''),
+    },
   },
-}))
+}));
 
-export default makeSource({ contentDirPath: 'posts', documentTypes: [Post] })
+export default makeSource({ contentDirPath: 'posts', documentTypes: [Post] });
