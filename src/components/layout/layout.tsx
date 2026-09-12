d477c46edb12e06fb1242e702fce11085f75d09f
João <joaovitorresende.jr@gmail.com>
2026-09-12 11:35:45 -0300

    chore: a little refactor to organized archive about slug to go to post-page

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,6 +1,7 @@
 import { Inter, PT_Sans_Caption } from 'next/font/google';
 import { Footer } from './footer';
 import { Header } from './header';
+import { CallToAction } from '@/Templates/landing-page/sections';
 
 type LayoutProps = {
   children: React.ReactNode;
@@ -23,6 +24,7 @@ export const Layout = ({ children }: LayoutProps) => {
     <div className={`${inter.className} ${ptSansCaption.className} relative flex min-h-screen flex-col bg-gray-700 font-inter`}>
       <Header />
       <main className="flex-1 flex flex-col mt-10 mb-12">{children}</main>
+      <CallToAction/>
       <Footer />
     </div>
   );
