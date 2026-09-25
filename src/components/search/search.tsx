f0fa95572cc1359bdd2e7bae51e5b5e75bff050d
João <joaovitorresende.jr@gmail.com>
2026-09-25 13:35:44 -0300

    feat: creating a not found page and updating search page

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -2,12 +2,14 @@
 import { cn } from "@/lib/utils";
 import { CircleX, SearchIcon } from "lucide-react"
 import { useRouter, useSearchParams } from "next/navigation";
-import { useCallback} from "react";
+import { useCallback, useEffect, useRef } from "react";
 
 export const Search = () => {
+    const inputRef = useRef<HTMLInputElement | null>(null)
     const router = useRouter();
     const searchParams = useSearchParams();
     const query = searchParams?.get('q') ?? '';
+    const hasQuery = !!searchParams?.has('q');
 
     const handleSearch = useCallback((event: React.FormEvent) => {
         event.preventDefault();
@@ -30,6 +32,12 @@ export const Search = () => {
         })
     }
 
+    useEffect(() => {
+        if (hasQuery) {
+            inputRef.current?.focus()
+        }
+    }, [hasQuery])
+
     return (
         <form onSubmit={handleSearch} className="relative group w-full md:w-60">
 
@@ -38,6 +46,7 @@ export const Search = () => {
                 query ? 'text-blue-300' : '')} />
 
             <input
+                ref={inputRef}
                 type="text"
                 placeholder="Buscar"
                 onChange={handleQueryChange}
